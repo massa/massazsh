954e9edca8e692ffe4ab34d19f417f2a4a9dde86
Humberto Massa <humbertomassa@gmail.com>
2017-10-23 10:54:31 -0200

    fzf nao funcionou

--- massa.sh
+++ massa.sh
@@ -66,17 +66,17 @@ do
 done
 
 
-# fzf ############
+# # fzf ############
 
-if _has fzf && _has ag; then
-  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
-  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_DEFAULT_OPTS='
-  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
-  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
-  '
-fi
+# if _has fzf && _has ag; then
+#   export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+#   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+#   export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+#   export FZF_DEFAULT_OPTS='
+#   --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
+#   --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+#   '
+# fi
 
 # https://github.com/dbbolton
 #
