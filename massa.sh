f41b3324eb891fbf20030e8c6988afe8a5d92ec4
Humberto Massa <humbertomassa@gmail.com>
2020-06-08 18:38:59 -0300

    changed everything out

--- massa.sh
+++ massa.sh
@@ -1,3 +1,68 @@
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+antigen bundles <<EOBUNDLES
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+  alias-finder
+  common-aliases
+
+  git
+  git-extras
+  git-flow
+  gitignore
+  github
+
+  rsync
+  python
+  perl
+
+  command-not-found
+  history
+  tmux
+
+  debian
+  fzf
+  brew
+  brew-cask
+  systemd
+  z
+
+  zsh-users/zsh-history-substring-search
+
+  Tarrasch/zsh-bd
+  Tarrasch/zsh-functional
+
+  rimraf/k
+
+# Syntax highlighting bundle.
+  zsh-users/zsh-syntax-highlighting
+
+# Completions
+  zsh-users/zsh-completions src
+  bobthecow/git-flow-completion
+  sheax0r/etcdctl-zsh
+  srijanshetty/zsh-pandoc-completion
+  RobSis/zsh-completion-generator
+
+EOBUNDLES
+
+# Load the theme.
+#antigen theme robbyrussell
+#antigen theme jreese
+#antigen theme agnoster
+
+export POWERLEVEL9K_MODE=nerdfont-complete
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon status command_execution_time history context dir dir_writable vcs)
+export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+export POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+
+antigen bundle bhilburn/powerlevel9k
+antigen theme bhilburn/powerlevel9k powerlevel9k
+
+# Tell antigen that you're done.
+
 alias a=alias
 alias h=history
 alias rm='rm -v'
