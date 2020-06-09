2abef35dca2c624b0b999a2cd6c159f726cfe52f
Humberto Massa <humbertomassa@gmail.com>
2020-06-08 23:09:16 -0300

    tmux in kitty

--- massa.sh
+++ massa.sh
@@ -37,6 +37,8 @@ alias s5='tmux neww -n  me  -t 6 "ssh me"'
 HISTIGNORE=$HISTIGNORE${HISTIGNORE+:}a:h:ls:ll:la:l:s9:s7:sx
 export EDITOR=nvim
 export VISUAL=nvim
+export ZSH_TMUX_UNICODE=true
+export ZSH_TMUX_AUTOCONNECT=true
 
 if [ -x "/usr/libexec/path_helper" ]; then
   eval $(/usr/libexec/path_helper)
