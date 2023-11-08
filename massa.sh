9fe0320956d461057f29f0a4348db9465e48a8ff
Humberto Massa <humbertomassa@gmail.com>
2023-11-08 15:38:48 -0300

    change tmux for zellij part 3

--- massa.sh
+++ massa.sh
@@ -45,6 +45,7 @@ HISTIGNORE=$HISTIGNORE${HISTIGNORE+:}a:h:ls:ll:la:l:s9:s7:sx
 export EDITOR=nvim
 export VISUAL=nvim
 export ZSH_ZELLIJ_AUTOSTART=true
+export ZSH_ZELLIJ_AUTOQUIT=false
 
 if [ -x "/usr/libexec/path_helper" ]; then
   eval $(/usr/libexec/path_helper)
